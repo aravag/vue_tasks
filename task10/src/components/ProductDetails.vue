1a71ee7be7ff2892b29ca226eba230ace017684a
Vagan Arakelyan <vra.vagan@gmail.com>
2024-04-08 08:02:05 -0300

    Task is done

---
+++ task10/src/components/ProductDetails.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h3>{{ isAvailable }}</h3>
+        <p>{{ getProductName }} - {{ formattedPrice }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductDetails',
+
+    data() {
+        return {
+            product: {
+                name: 'Product Good',
+                price: 12,
+                available: true,
+            },
+        };
+    },
+    computed: {
+        formattedPrice() {
+            let formatter = new Intl.NumberFormat("ru", {
+                style: "currency",
+                currency: "USD"
+            });
+            return formatter.format(this.product.price);
+        },
+        isAvailable() {
+            return this.product.available ? 'Available' : 'Out of stock';
+        },
+        getProductName() {
+            return this.product.name;
+        }
+    }
+};
+</script>
